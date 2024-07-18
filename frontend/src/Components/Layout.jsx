3565daaf1ce0e44377ba2c04c0851c31e04d7ccc
bleatingchef <ananddurgesh701@gmail.com>
2024-07-18 15:15:01 +0530

    UI

---
+++ frontend/src/Components/Layout.jsx
@@ -0,0 +1,17 @@
+// Layout.js
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="content">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,9 @@ export default {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      fill: ['hover'],
+    },
   },
   plugins: [require('daisyui'),],
 }
